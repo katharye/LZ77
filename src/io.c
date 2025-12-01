e32d18088662bdc878e9fa1d9fcbd34f282779cd
katharye <multiden007@gmail.com>
2025-12-01 20:54:59 +0300

    Added functions for reading a file into memory and writing a token to the output file.

--- src/io.c
+++ src/io.c
@@ -1,25 +1,58 @@
 #include "../include/io.h"
-#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
 
-unsigned char get_byte_from_dict(Dict *d, size_t pos) {
-    return d->window[pos % WINDOW_SIZE];
+unsigned char* put_file_in_data(const char* filename, size_t* file_size, int* all_okay) {
+    FILE *file = fopen(filename, "rb");
+
+    *all_okay = 0;
+    if (!file) return NULL;
+
+    if(fseek(file, 0, SEEK_END)) {
+        fclose(file);
+        return NULL;
+    }
+    
+    long pos = ftell(file);
+    if (pos <= 0) {
+        fclose(file);
+        return NULL;
+    }
+    
+    *file_size = (size_t)pos;
+
+    if(fseek(file, 0, SEEK_SET)) {
+        fclose(file);
+        return NULL;
+    }
+
+    unsigned char* data = malloc(sizeof(char) * (*file_size));
+    if (data == NULL) {
+        fclose(file);
+        return NULL;
+    }
+
+    size_t read_count = fread(data, 1, *file_size, file);
+    if (read_count != *file_size) {
+        free(data);
+        return NULL;
+    }
+
+    fclose(file);
+
+    *all_okay = 1;
+    return data;
 }
 
-void put_byte_in_dict(Dict *d, unsigned char c) {
-    d->window[d->pos % WINDOW_SIZE] = c;
-    d->pos++;
-    if (d->size < WINDOW_SIZE) d->size++;
+int put_data_in_file(FILE* file, uint16_t length, uint16_t value) {
+    uint16_t token;
+
+    token = (length & 0xF) | ((value & 0xFFF) << 4);
+    
+    size_t result = fwrite(&token, sizeof(uint16_t), 1, file);
+    if (result != 1) {
+        return 1;
+    }
+
+    return 0;
 }
-
-unsigned char dict_get_offset(Dict *d, size_t offset) {
-    size_t index = (d->pos - offset + WINDOW_SIZE) % WINDOW_SIZE;
-
-    return d->window[index];
-}
-
-void init_dict(Dict *d) {
-    d->pos = 0;
-    d->size = 0;
-    memset(d->window, 0, WINDOW_SIZE);
-}
-
